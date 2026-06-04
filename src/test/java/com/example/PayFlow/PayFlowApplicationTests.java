41bde731a41424444732848e125ade13959349ca
saurabhKl16 <saurabhlom1998@gmail.com>
2026-06-04 08:45:40 +0530

    project setup

---
+++ src/test/java/com/example/PayFlow/PayFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PayFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
